ef3721cec12b999b67c2e117cdcb54e51086452f
shamylkashif <144819710+shamylkashif@users.noreply.github.com>
2025-04-06 13:27:59 +0500

    Commit

--- lib/SubScreens/search-result.dart
+++ lib/SubScreens/search-result.dart
@@ -23,8 +23,6 @@ class _SearchResultsScreenState extends State<SearchResultsScreen> {
   @override
   Widget build(BuildContext context) {
     print("Search results length: ${widget.results.length}");
-    print("Search results length: ${widget.results.length}");
-    print("Search results length: ${widget.results.length}");
     return Scaffold(
       backgroundColor: background,
       appBar: AppBar(
